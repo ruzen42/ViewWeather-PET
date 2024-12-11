90afba2fb29977f0efca6987366a91e1ec647598
Deny ruzen <malledeferlive@gmail.com>
2024-12-11 22:04:38 +0500

    refactor code

---
+++ Class.cs
@@ -0,0 +1,83 @@
+﻿using Gtk;
+using Newtonsoft.Json.Linq;
+using DotNetEnv;
+
+namespace ShowWeather
+{
+    public class WeatherApp : Window
+    {
+        // City entry
+        private Entry _cityinput;
+        private Label _temperatureLabel;
+        private readonly string? _apikey;
+
+        public WeatherApp() : base("WhatWeather")
+        {
+            Env.Load();
+            _apikey = Environment.GetEnvironmentVariable("OPENWEATHERMAP_API_KEY");
+            SetDefaultSize(300, 110);
+            SetPosition(WindowPosition.Center);
+
+            _cityinput = new Entry { PlaceholderText = "Enter city here" };
+            var getWeatherButton = new Button("Start");
+
+            // This function is called when the getTheWeather button is clicked
+            getWeatherButton.Clicked += GetTheWeather;
+
+            _temperatureLabel = new Label();
+
+            // Create vertical box container
+            var vertbox = new Box(Orientation.Vertical, 0);
+            vertbox.PackStart(_cityinput, false, false, 5);
+            vertbox.PackStart(getWeatherButton, false, false, 5);
+            vertbox.PackStart(_temperatureLabel, false, false, 5);
+
+            Add(vertbox);
+            ShowAll();
+        }
+
+        private async void GetTheWeather(object? sender, EventArgs e)
+        {
+            string currentcity = _cityinput.Text;
+            string WeatherInfo = "";
+
+            if (string.IsNullOrEmpty(currentcity))
+            {
+                _temperatureLabel.Text = "NULL";
+                return;
+            }
+
+            try
+            {
+                WeatherInfo = await GetStringAsync(currentcity);
+            }
+            catch (Exception ex)
+            {
+                _temperatureLabel.Text = $"{currentcity} not exist";
+                Console.WriteLine(ex.Message);
+            }
+
+            _temperatureLabel.Text = WeatherInfo;
+        }
+
+        private async Task<string> GetStringAsync(string city)
+        {
+            string url = $"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={_apikey}&units=metric";
+
+            var client = new HttpClient();
+            var response = await client.GetAsync(url);
+            response.EnsureSuccessStatusCode();
+
+            string responseBody = await response.Content.ReadAsStringAsync();
+            var weatherData = JObject.Parse(responseBody);
+
+            string description;
+            string temp;
+
+            description = weatherData["weather"]?[0]?["description"]?.ToString() ?? "NULL";
+            temp = weatherData["main"]?["temp"]?.ToString() ?? "NULL";
+
+            return $"Weather in {city}: {description}, {temp} °C";
+        }
+    }
+}
--- obj/Debug/net8.0/Show-Me-the-Weather.AssemblyInfo.cs
+++ obj/Debug/net8.0/Show-Me-the-Weather.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Show-Me-the-Weather")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fbf093e0d5a66b18dcb724c48b0dbbf17af75167")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d4723bb1ccaaaac9b2fd41902e65cbb49c87d6b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Show-Me-the-Weather")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Show-Me-the-Weather")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
